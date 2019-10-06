f0fdeeb8cd25f719a04edc17dcc306527a2df99b
khanhhuynh265 <khanh.huynh.110@gmail>
2019-10-06 20:50:27 +0700

    upload 1

---
+++ Article/Model/ResourceModel/Article/Collection.php
@@ -0,0 +1,16 @@
+<?php
+namespace Smartosc\Article\Model\ResourceModel\Article;
+
+class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
+{
+    /**
+     * Define model & resource model
+     */
+    protected function _construct()
+    {
+        $this->_init(
+            'Smartosc\Article\Model\Article',
+            'Smartosc\Article\Model\ResourceModel\Article'
+        );
+    }
+}
